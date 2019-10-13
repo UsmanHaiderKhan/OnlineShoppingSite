55c9ea668e9e9de58336ea0de000df6f7d001cd0
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2019-10-13 10:36:19 +0500

    Update added dash board file functionality added later

--- admin/DashBoard.php
+++ admin/DashBoard.php
@@ -1,4 +1,7 @@
-<?php include "./header.php" ?>
+<?php
+    session_start();
+    include "./header.php"
+?>
 <?php include "./Sidebar.php" ?>
         
         <div class="grid_10">
@@ -6,14 +9,13 @@
                 <h2>
                     Product Sales</h2>
                 <p>
+
                     Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime error nam ducimus non, ratione odio
                     sapiente ex distinctio iure explicabo corrupti. Officia suscipit quas fuga exercitationem ratione
                     perspiciatis illum eos!
                 </p>
             </div>
-
         </div>
-
         <div class="clear">
         </div>
 </div>
