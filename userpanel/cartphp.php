9c9c527eae6f6c19f0682c0f00c3c0fd39a4d8e4
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2019-10-29 19:04:10 +0500

    Update the cart Page

--- userpanel/cartphp.php
+++ userpanel/cartphp.php
@@ -1,68 +1,96 @@
 <?php
 session_start();
 require_once '../admin/connection.php';
+require_once './function.php';
 ?>
-
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-   <style>
-       .shopping-cart{
-           padding: 3% 0;
-       }
-   </style>
-    <title>Shopping | Cart</title>
-</head>
-<body class="bg-light">
-
-               <?php
-
-                   require_once "topbar.php";
-               ?>
-
-             <div class="container-fluid">
-                 <div class="row px-5">
-                     <div class="col-lg-8">
-                         <div class="shopping-cart">
-                             <h4 class="my-5">My Product Cart</h4>
-                             <hr>
-                           <?php
-                           $productid=array_column($_SESSION['cart'],"productid");
-                           $sql="select * from product";
-                           $statement=$conn->prepare($sql);
-                           $result=$statement->execute();
-                               if ( $result)
-                               {
-                                   foreach ($productid as $id)
-                                   {
-                                       if ($result['id'] == $id)
-                                       {
-                                           cartProduct($result['product_name'],$result["product_price"],$result["product_image"]);
-                                       }
-                                   }
-                               }
-                           ?>
-                         </div>
-                     </div>
-                 </div>
-             </div>
+    <?php
+           if (isset($_POST["delete"]))
+           {
+               if ($_GET['action']=='delete')
+               {
+                   foreach ($_SESSION['cart'] as $key => $value)
+                   {
+                       if ($value['productid']==$_GET['id'])
+                       {
+                          unset($_SESSION['cart'][$key]);
+                          echo "<script> alert('Product Has Been Removed'); </script>";
+                       }
+                   }
+               }
+           }
+    ?>
 
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-        crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-        crossorigin="anonymous"></script>
-</body>
-</html>
+<?php
+require_once "topbar.php";
+?>
+<div class="container-fluid">
+    <div class="row px-5">
+        <div class="col-lg-7">
+            <div class="shopping-cart">
+                <h4 class="my-5">My Product Cart</h4>
+                <hr>
+                <?php
+                $total=0;
+                if (isset($_SESSION['cart'])) {
+                    $productid = array_column($_SESSION['cart'], "productid");
+                    $sql = "select * from product";
+                    $statement = $conn->prepare($sql);
+                    $result = $statement->execute();
+                    if ($result) {
+                        foreach ($statement as $value) {
+                            foreach ($productid as $id) {
+                                if ($value['id'] == $id) {
+                                    cartProduct($value["product_image"], $value['product_name'], $value["product_price"],$value["id"]);
+                                   $total=$total+ (int)$value['product_price'];
+                                }
+                            }
+                        }
+                    }
+                }
+                else {
+                    echo "<h5> Cart is Empty...! </h5>";
+                }
+                ?>
+
+                <div>
+                    <a href="checkout.php" name="checkout" class="btn btn-outline-success btn-block text-uppercase">Go to CheckOut</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 offset-lg-1 border round bg-white  mt-5 h-25">
+            <div class="mt-5">
+                <h6>Price Details</h6>
+                <hr>
+                <div class="row price-details">
+                    <div class="col-md-6">
+                        <?php
+                        if (isset($_SESSION["cart"]))
+                        {
+                            $count=count($_SESSION['cart']);
+                            echo "<h6>Price ($count items)</h6>";
+                        }else{
+                            echo  "<h6>Price (0 items)</h6>";
+                        }
+                        ?>
+                        <h6>Delivery Charges</h6>
+                        <hr>
+                        <h6>Amount Payable</h6>
+                    </div>
+                    <div class="col-md-6">
+                           <h6><?php echo  $total;?>Rs-/</h6>
+                        <h6 class="text-success">FREE</h6>
+                        <hr>
+                        <h6>
+                            <?php
+                            echo  $total;
+                            ?>Rs-/
+                        </h6>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php require_once "footer.php"?>
