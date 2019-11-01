41108586faaea41d35055899c4a61e79bfc2c278
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2019-11-01 17:14:02 +0500

    Update added Module for Paypal Integration or Payment Method

---
+++ userpanel/Payment_Success.php
@@ -0,0 +1,10 @@
+<?php
+//require_once '../admin/connection.php';
+//?>
+<?php
+//  $id=$_GET['id'];
+//  $sql="select * from checkout where id='$id'  "
+//
+//
+//?>
+<h3>Payment Has Been Done SuccessFully</h3>
