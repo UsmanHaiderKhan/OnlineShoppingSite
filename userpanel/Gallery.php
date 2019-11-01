e7fdc654667beac53e985d1fc029994b4c74e92b
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2019-11-01 18:41:03 +0500

    update added gallery file

---
+++ userpanel/Gallery.php
@@ -0,0 +1,51 @@
+<div class="container">
+    <h2 class="gallery-title text-center">Awesome Gallery</h2>
+    <div class="row no-gutters mt-3">
+        <div class="col-md-6 p-1">
+            <img src="./banner/Slider/2.jpg" class="w-100 img1"  alt="">
+        </div>
+        <div class="col-md-6 p-1">
+            <div class="row no-gutters">
+                <div class="col-md-6 p-1">
+                    <img src="./banner/Slider/3.jpg" class="w-100 img2"  alt=""/>
+                </div>
+                <div class="col-md-6 p-1">
+                    <img src="./banner/Slider/4.jpg" class="w-100 img3"  alt="">
+                </div>
+                <div class="col-md-6 p-1">
+                    <img src="./banner/Slider/5.jpg" class="w-100 img4"  alt="">
+                </div>
+                <div class="col-md-6 p-1">
+                    <img src="./banner/Slider/6.jpeg" class="w-100 img5"  alt="">
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+        <div class="row no-gutters">
+            <div class="col-md-6 p-1">
+                <img src="./banner/Slider/8.jpg" class="w-100 img6" alt="">
+            </div>
+            <div class="col-md-6 p-1">
+                <div class="row no-gutters">
+                    <div class="col-md-6  p-1">
+                        <img src="./banner/Slider/9.jpg" class="w-100 img7"  alt="">
+                    </div>
+                    <div class="col-md-6 p-1">
+                        <img src="./banner/Slider/1.jpg" class="w-100 img8"  alt="">
+                    </div>
+                    <div class="col-md-6 p-1">
+                        <img src="./banner/Slider/2.jpg" class="w-100 img9"  alt="">
+                    </div>
+                    <div class="col-md-6 p-1">
+                        <img src="./banner/Slider/8.jpg" class="w-100 img10"  alt="">
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
+    </div>
+</div>
