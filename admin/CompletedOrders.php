74faf2c145de48a20bda87010ff9fe8f85c5c16c
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2019-11-04 18:45:44 +0500

    Update Showing the all the order which Delivered to the users

---
+++ admin/CompletedOrders.php
@@ -0,0 +1,48 @@
+<?php
+require_once 'connection.php';
+require_once 'header.php'
+?>
+<?php require_once 'Sidebar.php'; ?>
+    <div class="col-md-9">
+
+    <div class="table-responsive">
+    <table class="table table-striped">
+    <thead>
+    <tr class="bg-dark text-white text-center">
+        <th>Image</th>
+        <th>Name</th>
+        <th>Price</th>
+        <th>qty</th>
+        <th>total</th>
+        <th>User</th>
+        <th>order_Status</th>
+        <th>Action</th>
+    </tr>
+    </thead>
+    <tbody class="card-text-center">
+<?php
+    $sql = "select * from checkout where order_status='ON'";
+    $statement = $conn->prepare($sql);
+    $result = $statement->execute();
+    if ($result) {
+    foreach ($statement as $value) {
+        ?>
+        <tr class="text-center">
+            <td><img src="./<?php echo $value['pro_image'] ?>" width="70" height="70" alt=""></td>
+            <td><?php echo $value['pro_name']; ?></td>
+            <td><?php echo $value['pro_price']; ?></td>
+            <td><?php echo $value['pro_qty']; ?></td>
+            <td><?php echo $value['pro_total']; ?></td>
+
+            <td><?php echo $value['user_id']; ?></td>
+            <td><a href="uncomplete.php?id=<?php echo $value['id']; ?>" class="btn btn-danger">Make UnComplete</a></td>
+            <td><a href="./DeleteOrder.php?id=<?php echo $value['id']; ?>" name="delete">Delete</a></td>
+        </tr>
+        </tbody>
+
+
+        <?php
+    }
+}?>
+    </table>
+    </div>
